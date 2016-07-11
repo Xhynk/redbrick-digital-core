c52a0c0de506fec2337cb90fb7e152a8b1e7b32f
alexdemchak <alexdemchak@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-07-11 22:20:30 +0000

    Starting to update plugin with better comment practices and more consistent architecture.
    git-svn-id: https://plugins.svn.wordpress.org/redbrick-digital-core/trunk@1452999 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-=== Redbrick Digital Core - Plugin ===
+=== Redbrick Digital Core ===
 Contributors: RedbrickDigital
 Tags: Review Engine
 Requires at least: 3.5.1
@@ -64,18 +64,29 @@ To enable the shortcodes and widgets, first go to the RBD Core menu item that's 
 Now go into any Page, Post, or Custom Post Type that has a content editor. You'll now have an Add Review Engine Display button next to Add Media. Click on that to access the shortcode editing popup.
 
 **URL**: Adjust your threshold to 3, 4, or 5 stars. *Note: This will only grab published reviews.*
+
 **Title**: An option H2 tag to preface your reviews section with.
+
 **Threshold**: Set this to 3, 4, or 5 stars to grab published reviews at and above that threshold.
+
 **Service**: Choose which service category to grab reviews from, default "All".
+
 **Staff**: Query against reviews with this staff member, default "All".
+
 **Location**: Query against reviews from this location, default "All".
+
 **Limit Characters**: Choose how many characters will show before the Read More link appears (if above the character limit).
+
 **Reviews Per Page**: Choose how many reviews show before the Show More Reviews button appears (if you have more reviews than are returned).
+
 **Max Columns**: Determine how many columns of reviews there are. *Note: 3 often works best for full width pages, and 2 for pages with sidebars.*
+
 **Hide X**: Choose to hide some Meta Data. *Note: City, Location, Staff, and Category are currently inoperable.*
-**Advanced Options**:
+
 **Enable Ajax**: Currently inoperable.
+
 **Hide Overview**: Hide the overview and aggregate score.
+
 **Disable CSS**: Will remove some non-structural CSS rules.
 
 *Click the Insert Into {Post Type} button and then click publish or update.*
@@ -85,14 +96,25 @@ Now go into any Page, Post, or Custom Post Type that has a content editor. You'l
 In your Appearance > Widgets page, there is a Review Slider widget. Drag that into the widget area you'd like to display the slider on. You may have more than one slider on a page if you wish.
 
 **Title**: Give the widget an optional title.
+
 **Review Engine URL**: Your Review Engine's URL. *Note: This is predefined, but can be changed.*
+
 **Threshold**: Set this to 3, 4, or 5 stars to grab published reviews at and above that threshold.
+
 **Review Count**: Choose how many reviews will be in the slider.
+
 **Character Count**: Choose how many characters will show before the Read More link appears (if above the character limit).
+
 **Service**: Choose which service category to grab reviews from, default "All".
+
 **Staff**: Query against reviews with this staff member, default "All".
+
 **Location**: Query against reviews from this location, default "All".
+
 **Hide Reviewer**: Hide the reviewer's name.
+
 **Hide Meta**: Hide the meta data under the review.
+
 **Disable CSS**: Will remove some non-structural CSS rules.
+
 **Slider Speed**: Will adjust the slider's speed, OFF will prevent all scrolling, and thus show only one review.
--- redbrick-digital-core.php
+++ redbrick-digital-core.php
@@ -2,103 +2,237 @@
 	/**
 	 * Redbrick Digital Core
 	 *
-	 * @package     RBD Core
+	 * @package     Redbrick Digital Core
 	 * @author      RedbrickDigital.net
 	 * @copyright   2016 RedbrickDigital.net
 	 * @license     GPL-2.0+
+	 * @link		https://plugins.svn.wordpress.org/redbrick-digital-core/
 	 *
 	 * @wordpress-plugin
-	 * Plugin Name: RBD Core
+	 * Plugin Name: Redbrick Digital Core
 	 * Description: This plugin enables Redbrick Digital Core usage, including the Review Engine Display shortcode, Review Slider widget, and Social Proof widget.
-	 * Version:     0.8.3
+	 * Version:     0.8.4
 	 * Author:      RedbrickDigital.net
 	 * Text Domain: rbd-core
 	 * License:     GPL-2.0+
 	 * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
-	 */
+	*/
 
-	// Load Core CSS
+/// <Code Block A>
+/// STANDARD FUNCTIONS
+
+	/**
+	 * Load Stylesheets
+	 *
+	 * @since 0.1
+	*/
 	add_action( 'wp_enqueue_scripts', 'rbd_core_load_css' );
 	function rbd_core_load_css(){
+		# Register Styles
 		wp_register_style( 'rbd-core', plugins_url( '/assets/css/core.css', __FILE__ ) );
 		wp_register_style( 'rbd-cutestrap-min', plugins_url( '/assets/css/cutestrap.min.css', __FILE__ ) );
+
+		# Enqueue Styles
 		wp_enqueue_style( 'rbd-widgets' );
 		wp_enqueue_style( 'rbd-core' );
 		wp_enqueue_style( 'rbd-cutestrap-min' );
 	}
 
-	// Load Core JS
+	/**
+	 * Load Javascript
+	 *
+	 * @since 0.1
+	*/
 	add_action( 'wp_enqueue_scripts', 'rbd_core_load_js' );
 	function rbd_core_load_js(){
-		// Unslider
+		# Unslider Plugin
 		wp_register_script( 'unslider-min', plugins_url( '/assets/js/unslider.min.js', __FILE__ ), array( 'jquery' ), '', true );
 		wp_enqueue_script( 'unslider-min' );
 
-		// Widgets JS
+		# Widget Javascript
 		wp_register_script( 'rbd-core-widgets', plugins_url( '/assets/js/widgets.js', __FILE__ ), array( 'jquery' ), '', true );
 		wp_enqueue_script( 'rbd-core-widgets' );
 	}
 
-	// Load Admin CSS
+	/**
+	 * Load Admin-side Stylesheets
+	 *
+	 * @since 0.1
+	*/
 	add_action( 'admin_enqueue_scripts', 'rbd_core_load_admin_css' );
 	function rbd_core_load_admin_css(){
+		# Register Styles
 		wp_register_style( 'rbd-admin', plugins_url( '/assets/css/admin.css', __FILE__ ) );
-		wp_enqueue_style( 'rbd-admin' );
 		wp_register_style( 'rbd-rangeslider', plugins_url( '/assets/plugins/rangeslider/rangeslider.css', __FILE__ ) );
+
+		# Enqueue Styles
+		wp_enqueue_style( 'rbd-admin' );
 		wp_enqueue_style( 'rbd-rangeslider' );
 	}
 
-	// Load Admin JS
+	/**
+	 * Load Admin-side Javascript
+	 *
+	 * @since 0.1
+	*/
 	add_action( 'admin_enqueue_scripts', 'rbd_core_conditional_add_media_js' );
 	function rbd_core_conditional_add_media_js(){
+		# Enqueue Styles
 		wp_enqueue_style( 'wp-color-picker' );
+
+		# Enqueue Scripts
     	wp_enqueue_script( 'wp-color-picker');
 		wp_enqueue_script( 'rbd-rangeslider', plugins_url( '/assets/plugins/rangeslider/rangeslider.min.js', __FILE__ ), array('jquery'), '1.0', true );
 		wp_enqueue_script( 'rbd-admin', plugins_url( '/assets/js/admin.js', __FILE__ ), array('jquery', 'rbd-rangeslider', 'wp-color-picker'), '1.0', true );
 	}
 
-	// Load Admin Pages
+	/**
+	 * Initialize Admin Settings Pages
+	 *
+	 * @since 0.3.5
+	 *
+	 * @internal { Instead of running `plugin_dir_path` for each page, and
+	 *	keeping track of them, we can just add any additional admin page slugs
+	 *	to the `$admin_pages` array. These files must be in the `/admin/` folder
+	 *	and named `admin-PAGENAME.php` to be included. }
+	*/
 	add_action( 'after_setup_theme', 'rbd_core_load_admin_pages' );
 	function rbd_core_load_admin_pages(){
-		$admin_pages = array( 'core' );
-		foreach($admin_pages as $admin_page){
+		# Define Admin Pages as Array
+		$admin_pages = array(
+			'core'
+		);
+
+		# Loop Through Array, Include Matched Pages
+		foreach( $admin_pages as $admin_page ){
 			include_once( plugin_dir_path( __FILE__ ) . 'admin/admin-'. $admin_page .'.php' );
 		}
 	}
 
-	// Load Widgets
+	/**
+	 * Initialize Widgets
+	 *
+	 * @since 0.2.1
+	 *
+	 * @internal { Each widget is loaded individually, and can be commented
+	 *	out for testing. Similar to "Initialize Admin Settings Pages", we loop
+	 *	through an array of widget slugs and load them from the `/widgets/`
+	 *	folder. Files must be named `widget-NAME.php` to be included. We also
+	 *	prevent loading until they put a Review Engine url in the Core Settings. }
+	*/
 	add_action( 'after_setup_theme', 'rbd_core_load_widgets' );
 	function rbd_core_load_widgets(){
-		$review_engine_url = get_option( 'rbd_core_review_engine_url' );
+		# Define Review Engine URL Option as Variable
+		$engine_url = get_option( 'rbd_core_review_engine_url' );
 
-		if( !empty( $review_engine_url ) ){
-			// Review Slider Widget
-			include( plugin_dir_path( __FILE__ ) . 'lib/widgets/widget-review-slider.php' );
+		# Define Widgets
+		$widgets = array(
+			'social-proof',
+			'review-slider'
+		);
 
-			// Reviews At a Glance (RAG)
-			include( plugin_dir_path( __FILE__ ) . 'lib/widgets/widget-social-proof.php' );
+		# If Review Engine URL Exists
+		/**
+		 * @TODO: Check if this is a real URL via an API calls
+		*/
+		if( !empty( $engine_url ) ){
+			# Loop Widgets and Include if Matched
+			foreach( $widgets as $widget ){
+				include( plugin_dir_path( __FILE__ ) . 'lib/widgets/widget-'. $widget .'.php' );
+			}
 		}
 	}
 
-	// Load Shortcodes
+	/**
+	 * Initialize Shortcodes
+	 *
+	 * @since 0.2.2
+	 *
+	 * @internal { Shortcodes are loaded exactly the same way as Widgets. The
+	 *	only difference is in the file functionality. @see: Initialize Widgets }
+	*/
 	add_action( 'after_setup_theme', 'rbd_core_load_shortcodes' );
 	function rbd_core_load_shortcodes(){
-		// Review Engine Display [rbd_review_engine]
-		include( plugin_dir_path( __FILE__ ) . 'lib/shortcodes/review-engine-display.php' );
+		# Define Review Engine URL Option as Variable
+		$engine_url = get_option( 'rbd_core_review_engine_url' );
+
+		# Define Shortcodes
+		$shortcodes = array(
+			'review-engine-display'
+		);
+
+		# If Review Engine URL Exists
+		/**
+		 * @TODO: Check if this is a real URL via an API calls
+		*/
+		if( !empty( $engine_url ) ){
+			# Loop Shortcodes and Include if Matched
+			foreach( $shortcodes as $shortcode ){
+				include( plugin_dir_path( __FILE__ ) . 'lib/shortcodes/shortcode-'. $shortcode .'.php' );
+			}
+		}
 	}
 
-	//Global Functions
-	function rbd_core_api_call(){
-		$api_string			= '/reviews-api-v2/?query_v2=true&user=thirdriverdev&key=56677c860f0f351a0a1b726b74f2f215&reviews_per_page=1';
-		$api_url			= 'http://'. str_replace( 'http://', '', get_option('rbd_core_review_engine_url') ) . $api_string;
+/// <Code Block B>
+/// LARGE SCOPE FUNCTIONS
 
+	/**
+	 * Reconstruct Review Engine URL
+	 *
+	 * @since 0.8.3
+	 *
+	 * @internal { I have to `str_replace` the URL sooo freaking much. I turned
+  	 *	it into it's own function so I don't have to double function each string. }
+	 *
+	 * @param Optional: Type. Indicates request for URL or API URL, otherwise
+	 *	returns standard URL.
+	 * @return A reconstructed URL so there's no fragment or missing protocol.
+	*/
+	function rbd_core_url( $rbd_core_url_args ){
+		# Force http:// and get the URL
+		$url		= get_option( 'rbd_core_review_engine_url' );
+		$protocol	= 'http://';
+		$_api_url	= '/reviews-api-v2/';
+		$_api_ver	= '?query_v2=true';
+		$_api_usr	= '&user=thirdriverdev';
+		$_api_key	= '&key=56677c860f0f351a0a1b726b74f2f215';
+		$_new_url	= str_replace( array( 'http://', 'https://' ), '', $url );
+
+		# See If User Wants API URL or Not and return
+		/**
+		 * @internal { We don't currently use `https://`, so lets remove it if a
+	 	 *	client adds it for some reason. }
+		*/
+		return ( $rbd_core_url_args == true ) ? $_new_url : $_new_url.$_api_url.$_api_ver.$_api_usr.$_api_key;
+	}
+
+	/**
+	 * Get Review Engine Info via API Call
+	 *
+	 * @since 0.7.5
+	 *
+	 * @internal { Sometimes we need to load in options from the client's
+	 *	Review Engine. Such as getting a list of Service Categories they have
+	 *	so we can build a dropdown menu of them for selection. }
+	 *
+	 * @uses rbd_core_url() - Returns Review Engine URL w/ or w/o API string.
+	 * @return Object from basic API Call
+	 * @example @see `/lib/shortcodes/shortcode-review-engine-display.php`
+	*/
+	function rbd_core_api_call(){
+		# Define Basic Light API String
+		/**
+		 * @TODO: Make NO reviews an Option
+		*/
+		$api_url = rbd_core_url( true ) .'&reviews_per_page=1';
+
+		# Check for `rbd_core_api_call`, set and use if doesn't exist
 		if ( false === ( $rbd_core_api_call = get_transient( 'rbd_core_api_call' ) ) ) {
 			$rbd_core_api_call = @file_get_contents( $api_url );
 			set_transient( 'rbd_core_api_call', $rbd_core_api_call, 86400 );
 		}
 
-		$data_object = json_decode( get_transient( 'rbd_core_api_call' ) );
-
-		return $data_object;
+		# Return JSON Decoded transient as an object
+		return json_decode( get_transient( 'rbd_core_api_call' ) );
 	}
 ?>
